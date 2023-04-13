cb7f53d3c04e143aa0effc1c1120a31e185b21dd
Michael Berger <m.gonser@icloud.com>
2023-04-13 11:09:02 +0200

    chore: setup demo

---
+++ packages/demo/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule],
+  selector: 'demo-root',
+  template: `<demo-nx-welcome></demo-nx-welcome>
+    <router-outlet></router-outlet>`,
+  styles: [''],
+})
+export class AppComponent {
+  title = 'demo';
+}
